1667c6676464002aaa1d23c1eb3a814252d510d1
maniacs2006 <maniacsaction2018@gmail.com>
2024-07-23 18:53:47 +0500

    task1

---
+++ task1.py
@@ -0,0 +1,15 @@
+name_1: str = "Anton"
+name_2: str = "Beth"
+name_3: str = "Chen"
+name_4: str = "Drew"
+name_5: str = "Ethan"
+age_1: int = 21
+age_2: int = 27
+age_3: int = 47
+age_4: int = 68
+age_5: int = 47
+print(name_1,"is",age_1)
+print(name_2,"is",age_2)
+print(name_3,"is",age_3)
+print(name_4,"is",age_4)
+print(name_5,"is",age_5)
